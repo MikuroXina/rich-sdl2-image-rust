3ff9a2ee995e4f55127f4141667a1981fb38b162
MikuroXina <ryosukadnak@gmail.com>
2021-08-29 00:11:11 +0900

    fix: Fix typo

--- src/surface.rs
+++ src/surface.rs
@@ -43,8 +43,8 @@ impl ImgSurface {
         let file_name_cstr = CString::new(file_name).expect("file_name mus not be empty");
         let mode = CStr::from_bytes_with_nul(b"rb\0").unwrap();
         let fp = unsafe { bind::SDL_RWFromFile(file_name_cstr.as_ptr(), mode.as_ptr()) };
-        let file_type_cstr =
-            file_type.map(|file_type| CString::new(file_type).expect("file_name mus not be empty"));
+        let file_type_cstr = file_type
+            .map(|file_type| CString::new(file_type).expect("file_name must not be empty"));
         let ptr = unsafe {
             bind::IMG_LoadTyped_RW(
                 fp,
